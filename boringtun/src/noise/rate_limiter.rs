3acf1a272c7a785a5ad4a2f9aba9b86cb9311a91
Frederic Henrichs <frederic@tinkerforge.com>
2025-12-23 10:36:59 +0100

    fix compiler warnings

--- boringtun/src/noise/rate_limiter.rs
+++ boringtun/src/noise/rate_limiter.rs
@@ -15,7 +15,7 @@ use aead::{AeadInPlace, KeyInit};
 use chacha20poly1305::{Key, XChaCha20Poly1305};
 use parking_lot::Mutex;
 use rand_core::{OsRng, RngCore};
-use ring::constant_time::verify_slices_are_equal;
+use subtle::ConstantTimeEq;
 
 const COOKIE_REFRESH: u64 = 128; // Use 128 and not 120 so the compiler can optimize out the division
 const COOKIE_SIZE: usize = 16;
